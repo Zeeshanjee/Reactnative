138a1ce1b99b6d113005fe214739ef89c9c1aa04
unknown <zeeshanghalib@gmail.com>
2018-05-17 23:36:23 -0700

    Update

--- Zeeshan/components/AlbumList.js
+++ Zeeshan/components/AlbumList.js
@@ -2,17 +2,23 @@ import React, {components} from 'react';
 import{view,Text}from 'react-native';
 import axios from 'axios';
 
-class AlbumList extends  components{
+class AlbumList extends  components
+    state ={ albums:[]
+   };
     componentWillMount(){
         axios.get('https//:rallycoding.herokuapp.com/api/music_albums')
+            .then(response => this.setState({albums:response.data}));
     }
 
-    render() {
-        return (
-            <View>
-                <Text>AlbumList!</Text>
-            </View>
-        );
+    render()
+   {
+    console.log(this.state);
+    return (
+        <View>
+            <Text>AlbumList!</Text>
+        </View>
+    );
     }
-}
+
+
 export default AlbumList;
